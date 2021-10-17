535c53e394c34842f6f881af9a59779c8c0cd0ad
RAM111111 <r.r.99@windowslive.com>
2021-10-17 13:49:14 +0300

    first commit

---
+++ app/src/main/java/com/example/top10downloaderapp/MainActivity.kt
@@ -0,0 +1,83 @@
+package com.example.top10downloaderapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import java.net.HttpURLConnection
+
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.textcell.*
+import kotlinx.coroutines.*
+import kotlinx.coroutines.Dispatchers.IO
+import org.json.JSONObject
+import java.lang.Exception
+import java.net.URL
+
+class MainActivity : AppCompatActivity() {
+    lateinit var rv:RecyclerView
+    lateinit var btn:Button
+    val parser = XMLParser()
+
+    lateinit var al:List<AppName>
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        rv = findViewById(R.id.recyclerView)
+        btn = findViewById(R.id.button)
+        al = listOf()
+
+
+        btn.setOnClickListener {
+            requestApi()
+
+        }
+
+
+
+
+    }
+
+    private fun requestApi(){
+
+        CoroutineScope(IO).launch {
+            val data = async {
+                fetchdata()
+            }.await()
+            if (data.isNotEmpty())
+            {
+                updateRV(data)
+
+
+            }
+        }
+    }
+
+    private fun fetchdata():String{
+        var response=""
+        try {
+            response = URL("http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topfreeapplications/limit=10/xml").readText(Charsets.UTF_8)
+        }catch (e: Exception){
+            println("Error $e")
+        }
+        return response
+    }
+
+    private suspend fun updateRV(data:String){
+        withContext(Dispatchers.Main)
+        {
+
+            var nn = parser.parse(data.byteInputStream())
+            Log.d("nn","$nn")
+            al = nn
+            rv.adapter?.notifyDataSetChanged()
+            rv.adapter = MyAdapter(al)
+            rv.layoutManager = LinearLayoutManager(this@MainActivity)
+
+        }
+
+
+    }
+}
---
+++ app/src/main/java/com/example/top10downloaderapp/MyAdapter.kt
@@ -0,0 +1,29 @@
+package com.example.top10downloaderapp
+
+import android.graphics.Color
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.textcell.view.*
+
+class MyAdapter(var item:List<AppName>):RecyclerView.Adapter<MyAdapter.ItemViewHolder>() {
+    class ItemViewHolder(ItemView: View):RecyclerView.ViewHolder(ItemView) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        return ItemViewHolder(
+            LayoutInflater.from(parent.context).inflate(
+                R.layout.textcell,parent,false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        var items = item[position]
+        holder.itemView.tv.text =items.name
+    }
+
+    override fun getItemCount(): Int = item.size
+}
